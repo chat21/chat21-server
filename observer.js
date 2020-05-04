a69bfaad5243ee4b5abe62583053759c2dd38dc8
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-05-04 23:07:40 +0200

    jwt added

--- observer.js
+++ observer.js
@@ -14,13 +14,18 @@ var amqpConn = null;
 var exchange = 'amq.topic';
 const topic_outgoing = 'apps.tilechat.users.*.messages.*.outgoing'
 const topic_incoming = 'apps.tilechat.users.*.messages.*.incoming'
-const topic_presence = 'presence.#'
+const topic_presence = 'apps.tilechat.users.*.presence.*'
 
 var chatdb;
 
 function startMQ() {
   console.log("Starting AMQP chat server...")
-  amqp.connect('amqp://andrea:Freedom73@localhost:5672?heartbeat=60', function (err, conn) {
+  // amqp.connect('amqp://andrea:Freedom73@localhost:5672?heartbeat=60', function (err, conn) {
+  const userid = "ignored"
+  //const password = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYzA4MmU5Yy1hOWIxLTQ1ZGYtYWRkMy0zMGY3YzY3NWEwZmUiLCJzdWIiOiJ1bGlzc2UiLCJzY29wZSI6WyJyYWJiaXRtcS5yZWFkOiovKi91bGlzc2UuKiIsInJhYmJpdG1xLnJlYWQ6Ki8qL2dpYWd1YXJveC4qIiwicmFiYml0bXEud3JpdGU6Ki8qL3VsaXNzZS4qIiwicmFiYml0bXEuY29uZmlndXJlOiovKi8qIl0sImNsaWVudF9pZCI6InVsaXNzZSIsImNpZCI6InVsaXNzZSIsImF6cCI6InVsaXNzZSIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfaWQiOiJ1bGlzc2UiLCJvcmlnaW4iOiJ1YWEiLCJ1c2VyX25hbWUiOiJ1bGlzc2UiLCJlbWFpbCI6InJhYmJpdF9hbm9ueW1AZXhhbXBsZS5jb20iLCJhdXRoX3RpbWUiOjE1ODg1NDEwNzksImlhdCI6MTU4ODU0MTA3OSwiZXhwIjoxNTkxMTMzMDc5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvdWFhL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbInJhYmJpdG1xIiwidWxpc3NlIl0sImprdSI6Imh0dHBzOi8vbG9jYWxob3N0OjgwODAvdWFhL3Rva2VuX2tleXMiLCJraWQiOiJsZWdhY3ktdG9rZW4ta2V5In0.hm60kLv8EEW-Ih8gIIltNKTxtSt6l6zJBoaIsP1Rd64"
+  // const password = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2RkZjdkNC02ZTdjLTQ5NzEtYmY0My1jOTZkMTYzMjAwNTkiLCJzdWIiOiJ1bGlzc2UiLCJzY29wZSI6WyJyYWJiaXRtcS5yZWFkOiovKi9hcHBzLnRpbGVjaGF0LnVzZXJzLnVsaXNzZS4jIiwicmFiYml0bXEud3JpdGU6Ki8qL2FwcHMudGlsZWNoYXQudXNlcnMudWxpc3NlLiMiLCJyYWJiaXRtcS5jb25maWd1cmU6Ki8qLyoiXSwiY2xpZW50X2lkIjoidWxpc3NlIiwiY2lkIjoidWxpc3NlIiwiYXpwIjoidWxpc3NlIiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9pZCI6InVsaXNzZSIsIm9yaWdpbiI6InVhYSIsInVzZXJfbmFtZSI6InVsaXNzZSIsImF1dGhfdGltZSI6MTU4ODU4MDQ3NywiaWF0IjoxNTg4NTgwNDc3LCJleHAiOjE1OTExNzI0NzcsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC91YWEvb2F1dGgvdG9rZW4iLCJ6aWQiOiJ1YWEiLCJhdWQiOlsicmFiYml0bXEiLCJ1bGlzc2UiXSwiamt1IjoiaHR0cHM6Ly9sb2NhbGhvc3Q6ODA4MC91YWEvdG9rZW5fa2V5cyIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkifQ.rcrHL_FoOAJNxhF64gJqIN3nwqtqpvsG4Pg2CpCEoIg"
+  const password = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjYzQzZDZiZC1kNTY5LTRjZWMtOGJiZi00ZjA2ODY4Y2JlMWMiLCJzdWIiOiIwMS1PQlNFUlZFUiIsInNjb3BlIjpbInJhYmJpdG1xLnJlYWQ6Ki8qLyoiLCJyYWJiaXRtcS53cml0ZToqLyovKiIsInJhYmJpdG1xLmNvbmZpZ3VyZToqLyovKiJdLCJjbGllbnRfaWQiOiIwMS1PQlNFUlZFUiIsImNpZCI6IjAxLU9CU0VSVkVSIiwiYXpwIjoiMDEtT0JTRVJWRVIiLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJ1c2VyX2lkIjoiMDEtT0JTRVJWRVIiLCJvcmlnaW4iOiJ1YWEiLCJ1c2VyX25hbWUiOiIwMS1PQlNFUlZFUiIsImF1dGhfdGltZSI6MTU4ODU4NzQzMywiaWF0IjoxNTg4NTg3NDMzLCJleHAiOjE1OTExNzk0MzMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC91YWEvb2F1dGgvdG9rZW4iLCJ6aWQiOiJ1YWEiLCJhdWQiOlsicmFiYml0bXEiLCIwMS1PQlNFUlZFUiJdLCJqa3UiOiJodHRwczovL2xvY2FsaG9zdDo4MDgwL3VhYS90b2tlbl9rZXlzIiwia2lkIjoibGVnYWN5LXRva2VuLWtleSJ9.3SDis6B1SeLYCRcYRRHxeGp-bL0P56f0grORwfQiNes"
+  amqp.connect('amqp://' + userid + ':' + password + '@localhost:5672?heartbeat=60', function (err, conn) {
     if (err) {
       console.error("[AMQP]", err.message);
       return setTimeout(startMQ, 1000);
@@ -106,17 +111,22 @@ function startWorker() {
     });
     ch.assertQueue("jobs", { durable: true }, function (err, _ok) {
       if (closeOnErr(err)) return;
+      // const topic_outgoing_ = 'apps.tilechat.users.ulisse.#'
+      // ch.bindQueue(_ok.queue, exchange, topic_outgoing_, {}, function (err3, oka) {
+      //   console.log("bind: " + _ok.queue + " err: " + err3 + " topic: " + topic_outgoing_);
+      // });
+      // ch.bindQueue(_ok.queue, exchange, 'giaguarox.*', {}, function (err3, oka) {
+      //   console.log("bind: " + _ok.queue + " err: " + err3 + " topic: " + "giaguaro.*");
+      // });
+
       ch.bindQueue(_ok.queue, exchange, topic_outgoing, {}, function (err3, oka) {
         console.log("bind: " + _ok.queue + " err: " + err3 + " topic: " + topic_outgoing);
-        // console.log(oka)
       });
       ch.bindQueue(_ok.queue, exchange, topic_incoming, {}, function (err3, oka) {
         console.log("bind: " + _ok.queue + " err: " + err3 + " topic: " + topic_incoming);
-        // console.log(oka)
       });
       ch.bindQueue(_ok.queue, exchange, topic_presence, {}, function (err3, oka) {
         console.log("bind: " + _ok.queue + " err: " + err3 + " topic: " + topic_presence);
-        // console.log(oka)
       });
       ch.consume("jobs", processMsg, { noAck: false });
       console.log("Worker is started");
@@ -138,7 +148,7 @@ function processMsg(msg) {
 }
 
 function work(msg, callback) {
-  console.log("processing of:", msg.content.toString(), "topic:", msg.fields.routingKey);
+  console.log("Topic:", msg.fields.routingKey, " message:", msg.content.toString());
   const topic = msg.fields.routingKey //.replace(/[.]/g, '/');
   const message_string = msg.content.toString();
   if (topic.endsWith('.outgoing')) {
@@ -153,18 +163,9 @@ function work(msg, callback) {
   else {
     console.log("unkownn topic", topic)
   }
-
-  // var topic_parts = msg.fields.routingKey.split(".")
-  // const sender_id = topic_parts[3]
-  // const recipient_id = topic_parts[5]
-  // var dest_topic = 'apps.tilechat.users.' + recipient_id + '.conversations.' + sender_id 
-  // console.log("destinazione:",  dest_topic);
-  // publish(exchange, dest_topic, new Buffer(msg.content.toString()));
-  // callback(true);
-
 }
 
-// ***** TOPICS HANDLERS ******/
+// ***** TOPIC HANDLERS ******/
 
 function process_presence(topic, message_string, callback) {
   console.log("got PRESENCE testament", message_string, " on topic", topic)
@@ -185,23 +186,29 @@ function process_outgoing(topic, message_string, callback) {
   var message = JSON.parse(message_string)
   var messageId = uuidv4();
   const now = Date.now()
-  var outgoing_message = {
-    message_id: messageId,
-    text: message.text,
-    sender_id: sender_id,
-    sender_fullname: message.sender_fullname,
-    recipient_id: recipient_id,
-    recipient_fullname: message.recipient_fullname,
-    channel_type: message.channel_type,
-    app_id: app_id,
-    timestamp: now
-  }
+  var outgoing_message = message;
+  outgoing_message.message_id = messageId
+  outgoing_message.sender = sender_id
+  outgoing_message.recipient = recipient_id
+  outgoing_message.app_id = app_id
+  outgoing_message.timestamp = now
+  // {
+  //   message_id: messageId,
+  //   text: message.text,
+  //   sender: sender_id,
+  //   sender_fullname: message.sender_fullname,
+  //   recipient: recipient_id,
+  //   recipient_fullname: message.recipient_fullname,
+  //   channel_type: message.channel_type,
+  //   app_id: app_id,
+  //   timestamp: now
+  // }
   
-  var newMessage = outgoing_message
-  newMessage.timelineOf = me
-  newMessage.status = MessageConstants.CHAT_MESSAGE_STATUS.SENT
-  console.log("saving and forwarding message/conversation update:", newMessage)
-  chatdb.saveOrUpdateMessage(newMessage, function(err, msg) {
+  var savedMessage = outgoing_message
+  savedMessage.timelineOf = me
+  savedMessage.status = MessageConstants.CHAT_MESSAGE_STATUS.SENT
+  console.log("saving and forwarding message/conversation update:", savedMessage)
+  chatdb.saveOrUpdateMessage(savedMessage, function(err, msg) {
     console.log("error", err)
     const my_conversation_topic = 'apps.tilechat.users.' + me + '.conversations.' + convers_with
     let conversation = outgoing_message
@@ -235,17 +242,18 @@ function process_incoming(topic, message_string, callback) {
   const me = recipient_id
 
   var incoming_message = JSON.parse(message_string)
-  var newMessage = incoming_message
-  newMessage.timelineOf = me
-  newMessage.path = topic
-  newMessage.status = MessageConstants.CHAT_MESSAGE_STATUS.SENT
+  var savedMessage = incoming_message
+  savedMessage.timelineOf = me
+  savedMessage.status = MessageConstants.CHAT_MESSAGE_STATUS.SENT
     
-  console.log("saving incoming message/conversation update:", newMessage)
-  chatdb.saveOrUpdateMessage(newMessage, function(err, msg) {
+  console.log("saving incoming message/conversation update:", savedMessage)
+  chatdb.saveOrUpdateMessage(savedMessage, function(err, msg) {
     const my_conversation_topic = 'apps.tilechat.users.' + me + '.conversations.' + convers_with
     let conversation = incoming_message
-    conversation.conversWith = convers_with
+    conversation.conversWith = convers_with // new!
+    conversation.key = convers_with // retro comp
     conversation.is_new = true
+    conversation.last_message_text = conversation.text // retro comp
     const conversation_payload = JSON.stringify(conversation)
     publish(exchange, my_conversation_topic, Buffer.from(conversation_payload), function(err) {
       if (err) {
