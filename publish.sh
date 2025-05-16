59d06b2d0f08aca416da3f5ab6b343a9c8043514
Gabriele Panico <gabriele.panico@gmail.com>
2025-05-16 10:50:09 +0200

    publish changed

--- publish.sh
+++ publish.sh
@@ -2,9 +2,20 @@
 version=`node -e 'console.log(require("./package.json").version)'`
 echo "version $version"
 
+npm i
+
+# Get curent branch name
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+remote_name=$(git config --get branch.$current_branch.remote)
+
+## Push commit to git
+git add .
+git commit -m "version added: ### $version"
+git push "$remote_name" "$current_branch"
+
 if [ "$version" != "" ]; then
     git tag -a "$version" -m "`git log -1 --format=%s`"
-    echo "Created a new tag, $version"
     git push --tags
+    echo "Created a new tag, $version"
     npm publish --access public
 fi
